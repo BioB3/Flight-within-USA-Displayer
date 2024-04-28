2964b54136cdeed45992bb65ca46250083aff73f
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-04-28 15:55:25 +0700

    apply Observer and State pattern

--- model.py
+++ model.py
@@ -1,10 +1,60 @@
 """Model to handle data processing for Flight within USA displayer"""
+from __future__ import annotations
 import os
 import pandas as pd
+from abc import ABC, abstractmethod
 
-class Model:
+class Subject(ABC):
+    """
+    Abstract class for subject to be observed
+    """
+    @abstractmethod
+    def attach(self, observer: Observer):
+        """
+        Attach an observer to the subject
+        """
+        pass
+    
+    @abstractmethod
+    def detach(self, observer: Observer):
+        """
+        Detach an observer from the subject
+        """
+        pass
+    
+    @abstractmethod
+    def notify(self):
+        """
+        Notify observers about an event
+        """
+        pass
+
+class Observer(ABC):
+    """
+    Abstract class for observer
+    """
+    @abstractmethod
+    def update(self, subject: Subject):
+        """
+        Recieve update from subject
+        """
+        pass
+
+class Flight_Data_Model(Subject):
+    """
+    Model for computing data from the datasets
+    """
+    _observers : list[Observer] =  []
+    
     def __init__(self) -> None:
         self.__df = self.gen_df()
+        self.__avg_delay: pd.DataFrame
+        self.__states = [Search_by_Flight(self.__df)]
+        self.__current_state = self.__states[0]
+
+    @property
+    def avg_delay(self):
+        return self.__avg_delay
 
     def gen_df(self):
         """
@@ -27,6 +77,7 @@ class Model:
                                  ("Evening" if 1600 <= x < 1900 else
                                   ("Night" if x >= 1900 or x < 400 else None))))
                                for x in df3["DEP_TIME"]]
+        df3["WEEK"] = df3["FL_DATE"].dt.isocalendar().week
         df3["STATUS"] = ["Delayed" if x1 else
                          ("Diverted" if x2 else
                           ("Canceled" if x3 else "On-time"))
@@ -36,7 +87,93 @@ class Model:
         df3.drop(df3.columns[df3.columns.str.contains('unnamed',case = False)],
                        axis=1, inplace=True)
         df3.drop(["AIRLINE_ID","ORIGIN_AIRPORT","DEST_AIRPORT"], axis=1, inplace=True)
+        return df3
+    
+    def attach(self, observer: Observer):
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer):
+        self._observers.remove(observer)
+
+    def notify(self):
+        for observer in self._observers:
+            observer.update(self)
+    
+    def set_state(self, index: int):
+        self.__current_state = self.__states[index]
+
+    def get_avg_delay_data(self, flight: list[str], week: list[bool],
+                         time_blk: list[bool]):
+        self.__avg_delay = self.__current_state.create_avg_delay(flight, week, time_blk)
+
+class Search_state(ABC):
+    """
+    Abstract class for state that contain the model's function
+    """
+    _time_blk_dict = {0: "Early Morning",
+                      1: "Morning",
+                      2: "Afternoon",
+                      3: "Evening",
+                      4: "Night"}
+    
+    def __init__(self, dataframe: pd.DataFrame) -> None:
+        self._df = dataframe
+    
+    @property
+    def df(self):
+        """
+        Getter for df attribute
+        """
+        return self._df
+    
+    @abstractmethod
+    def create_avg_delay(self):
+        """
+        Create a dataframe with data needed to plot a graph of average delay time
+        """
+        pass
+    
+    @abstractmethod
+    def create_pencent_on_time(self):
+        """
+        Create a dataframe with data needed to plot a graph of percentage of flight
+        departing on-time, with delay, diverted, or canceled
+        """
+        pass
+    
+    @abstractmethod
+    def create_percent_time_blk(self):
+        """
+        Create a dataframe with data needed to plot a graph of percentage of flight
+        in each time block
+        """
+        pass
+
+class Search_by_Flight(Search_state):
+    def __init__(self, dataframe: pd.DataFrame) -> None:
+        super().__init__(dataframe)
+
+    def create_avg_delay(self, flight: list[str], week: list[bool],
+                         time_blk: list[bool]):
+        selected_wk = [i+1 for i in range(5) if week[i]]
+        selected_time_blk = [self._time_blk_dict[i] for i in range(5) if time_blk[i]]
+        return self.df.loc[(self.df["ORIGIN"] == flight[0]) &
+                            (self.df["DEST"] == flight[1]) &
+                            (self.df["WEEK"].isin(selected_wk)) &
+                            (self.df["DEP_TIME_BLK"].isin(selected_time_blk))]
+    
+    def create_pencent_on_time(self, flight: list[str], week: list[bool],
+                         time_blk: list[bool]):
+        pass
+    
+    def create_percent_time_blk(self, flight: list[str], week: list[bool],
+                         time_blk: list[bool]):
+        pass
 
 
 if __name__ == "__main__":
-    data = Model()
+    data = Flight_Data_Model()
+    data.get_avg_delay_data(["TUL","ORD"],
+                            [True,False,False,False,False],
+                            [True,True,True,True,True])
+    print(data.avg_delay)
